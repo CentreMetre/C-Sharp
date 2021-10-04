ef9f1cb71ca393ede1f9424fdd3acc9a378f4ae5
Martin McLaren <61330791+LuckyMiner01@users.noreply.github.com>
2021-10-04 22:49:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PIN = 1234;
+
+            double PINch;
+
+            Console.WriteLine("Please input your PIN");
+
+            string PINin = Console.ReadLine();
+
+            PINch = Convert.ToDouble(PINin);
+
+            if (PINch == PIN)
+            {
+                Console.WriteLine("Correct PIN");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect PIN");
+            }
+        }
+    }
+}
