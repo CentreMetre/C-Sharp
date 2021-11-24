e410b58b2ec51eca1009820d9dca50d67307efb0
Martin McLaren <61330791+LuckyMiner01@users.noreply.github.com>
2021-11-24 13:45:40 +0000

    Winforms Wage Calculator

---
+++ BBC Bitesize wage calculator/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace BBC_Bitesize_wage_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbox_hours_worked = new System.Windows.Forms.TextBox();
+            this.lbl_hours_worked = new System.Windows.Forms.Label();
+            this.lbl_total_pay_label = new System.Windows.Forms.Label();
+            this.lbl_total_pay_output = new System.Windows.Forms.Label();
+            this.lbl_error_output = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbox_hours_worked
+            // 
+            this.txtbox_hours_worked.Location = new System.Drawing.Point(104, 55);
+            this.txtbox_hours_worked.Name = "txtbox_hours_worked";
+            this.txtbox_hours_worked.Size = new System.Drawing.Size(100, 23);
+            this.txtbox_hours_worked.TabIndex = 0;
+            this.txtbox_hours_worked.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbl_hours_worked
+            // 
+            this.lbl_hours_worked.AutoSize = true;
+            this.lbl_hours_worked.Location = new System.Drawing.Point(15, 58);
+            this.lbl_hours_worked.Name = "lbl_hours_worked";
+            this.lbl_hours_worked.Size = new System.Drawing.Size(86, 15);
+            this.lbl_hours_worked.TabIndex = 1;
+            this.lbl_hours_worked.Text = "Hours Worked:";
+            this.lbl_hours_worked.Click += new System.EventHandler(this.lbl_hours_worked_Click);
+            // 
+            // lbl_total_pay_label
+            // 
+            this.lbl_total_pay_label.AutoSize = true;
+            this.lbl_total_pay_label.Location = new System.Drawing.Point(43, 92);
+            this.lbl_total_pay_label.Name = "lbl_total_pay_label";
+            this.lbl_total_pay_label.Size = new System.Drawing.Size(58, 15);
+            this.lbl_total_pay_label.TabIndex = 2;
+            this.lbl_total_pay_label.Text = "Total Pay:";
+            // 
+            // lbl_total_pay_output
+            // 
+            this.lbl_total_pay_output.AutoSize = true;
+            this.lbl_total_pay_output.Location = new System.Drawing.Point(104, 92);
+            this.lbl_total_pay_output.Name = "lbl_total_pay_output";
+            this.lbl_total_pay_output.Size = new System.Drawing.Size(19, 15);
+            this.lbl_total_pay_output.TabIndex = 3;
+            this.lbl_total_pay_output.Text = "£0";
+            // 
+            // lbl_error_output
+            // 
+            this.lbl_error_output.Location = new System.Drawing.Point(15, 156);
+            this.lbl_error_output.Name = "lbl_error_output";
+            this.lbl_error_output.Size = new System.Drawing.Size(189, 42);
+            this.lbl_error_output.TabIndex = 4;
+            this.lbl_error_output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Wage Calculator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(72, 122);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(75, 23);
+            this.btn_calculate.TabIndex = 7;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 162);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_error_output);
+            this.Controls.Add(this.lbl_total_pay_output);
+            this.Controls.Add(this.lbl_total_pay_label);
+            this.Controls.Add(this.lbl_hours_worked);
+            this.Controls.Add(this.txtbox_hours_worked);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(234, 248);
+            this.MinimumSize = new System.Drawing.Size(234, 201);
+            this.Name = "Form1";
+            this.Text = "Wage Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbox_hours_worked;
+        private System.Windows.Forms.Label lbl_hours_worked;
+        private System.Windows.Forms.Label lbl_total_pay_label;
+        private System.Windows.Forms.Label lbl_total_pay_output;
+        private System.Windows.Forms.Label lbl_error_output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_calculate;
+    }
+}
+
---
+++ BBC Bitesize wage calculator/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBC_Bitesize_wage_calculator
+{
+    public partial class Form1 : Form
+    {
+        
+        /*void CalculateWage(double hoursWorked, ref double totalPay)
+        {
+            if (hoursWorked >= minHours && hoursWorked <= maxHours)
+            {
+                if (hoursWorked <= normalHours) //no overtime
+                {
+                    //no overtime
+                    double totalPayNoSym = hoursWorked * hourlyRate;
+                    string totalpay = "£" + Convert.ToString(totalPayNoSym);
+                    lbl_total_pay_output.Text = Convert.ToString(totalPay);
+                }
+                else if (hoursWorked > normalHours)
+                {
+                    //there is overtime
+                }
+                else
+                {
+                    //Something has gone seriously wrong!
+                }
+            }
+            else if (hoursWorked <= minHours || hoursWorked >= maxHours)
+            {
+                lbl_error_output.Text = "Sorry, that amount of hours isnt valid.";
+            }
+            else
+            {
+                //Something has gone seriously wrong!
+            }
+        }*/
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //constants
+        const double hourlyRate = 10.00;
+        const double overtimeRate = 15.00;
+        const int maxHours = 60;
+        const int minHours = 1;
+        const int normalHours = 40;
+
+        //non-constant variables
+        double hoursWorked = 0;
+        double overtimeHours = 0;
+        double normalPay;
+        double overtimePay = 0;
+        double totalPay = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //error not there, dimensions = 234, 201
+            //error there, dimensions = 234, 248
+        }
+
+        private void btn_test1_Click(object sender, EventArgs e)
+        {
+            //lbl_error_output.Text = "Sorry, that amount of hours isnt valid.";
+            //lbl_total_pay_output.Text = "£40";
+            lbl_total_pay_output.Text = txtbox_hours_worked.Text;
+        }
+
+        private void lbl_hours_worked_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        { 
+            //CalculateWage(hoursWorked, ref totalPay);
+            lbl_error_output.Text = "";
+            lbl_total_pay_output.Text = "£0";
+
+            try
+            {
+                hoursWorked = double.Parse(txtbox_hours_worked.Text);
+            }
+            catch (FormatException)
+            {
+                lbl_error_output.Text = "Sorry, but that input is invalid.";
+                Size = new Size(234, 248);
+                return;
+            }
+
+            if (hoursWorked >= minHours)
+            {
+                if (hoursWorked <= maxHours)
+                {
+                    if (hoursWorked <= normalHours) //no overtime
+                    {
+                        //no overtime
+                        normalPay = hoursWorked * hourlyRate;
+                        totalPay = normalPay;
+                        lbl_total_pay_output.Text = Convert.ToString("£" + totalPay);
+                    }
+                    else if (hoursWorked > normalHours) //There is overtime
+                    {
+                        //Minusing the overtime from the hours worked and wrking out the pay
+                        overtimeHours = hoursWorked - normalHours;
+                        overtimePay = overtimeHours * overtimeRate;
+
+                        //working out normal pay
+                        normalPay = normalHours * hourlyRate;
+                        
+                        //seting total pay
+                        totalPay = normalPay + overtimePay;
+                        lbl_total_pay_output.Text = Convert.ToString("£" + totalPay);
+                    }
+                    else
+                    {
+                        //Something has gone seriously wrong!
+                    }
+                }
+                else if (hoursWorked >= maxHours)
+                {
+                    lbl_error_output.Text = "";
+                    lbl_error_output.Text = "Sorry, that amount of hours is to many.";
+                }
+                else
+                {
+                    //Something has gone seriously wrong
+                }
+            }
+            else if (hoursWorked <= minHours)
+            {
+                lbl_error_output.Text = "";
+                lbl_error_output.Text = "Sorry, that amount of hours isn't enough.";
+            }
+            else
+            {
+                //Something has gone seriously wrong!
+            }
+
+            if (lbl_error_output.Text == "")
+            {
+                //error not there, dimensions = 234, 201
+                Size = new Size(234, 201);
+                
+            }
+            else if (lbl_error_output.Text != "")
+            {
+                //error there, dimensions = 234, 248
+                Size = new Size(234, 248);
+            }
+            else
+            {
+                //something is wrong 
+            }
+        }
+    }
+}
